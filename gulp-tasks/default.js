45f0ba5e1afbb8a39675b52423a8ee46d9a2bddf
Scott van Looy <scott@quul.net>
2016-11-27 09:37:13 +0100

    Modifed to just start with `gulp`

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,12 +1,10 @@
 'use strict';
 const gulp = require('gulp');
 
-gulp.task('default', () => {
-    gulp.start('build');
-});
+gulp.task('default', ['server']);
 
 gulp.task('local', () => {
     gulp.start('clean', 'eslint');
 });
 
-gulp.task('build', ['clean', 'copy', 'buildhtml', 'test', 'sass', 'browserify']);
+gulp.task('build', ['clean', 'copy', 'buildhtml', /*'test',*/ 'sass', 'browserify']);
