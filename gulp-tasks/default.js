e091dfc687825f246b61d92716e8b97f5ff1b9f3
Scott van Looy <scott@quul.net>
2019-05-12 09:04:50 +0200

    Update to Gulp 4. Deps updates

--- gulp-tasks/default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-
-gulp.task('default', ['server']);
-gulp.task('local', () => {
-    gulp.start('clean', 'eslint');
-});
-gulp.task('build', ['eslint', 'clean', 'copy', 'buildhtml', /*'test',*/ 'sass', 'browserify']);
