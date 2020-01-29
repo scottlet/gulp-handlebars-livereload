5263d1f7a4df4c81a34f498fd034d625202c105c
Scott van Looy <scott@quul.net>
2020-01-29 15:07:25 +0100

    Various tweaks and issues, Linting, updated deps

--- gulpfile.js/server.js
+++ gulpfile.js/server.js
@@ -1,4 +1,4 @@
-/*eslint-disable no-console*/
+const fancyLog = require('fancy-log');
 const connectLivereload = require('connect-livereload');
 const { GULP_PORT, LIVERELOAD_PORT } = require('./CONSTS');
 const { server } = require('gulp-connect');
@@ -18,7 +18,7 @@ function makeServer(cb) {
             ];
         }
     });
-    console.log('server http://127.0.0.1:' + GULP_PORT);
+    fancyLog('server http://127.0.0.1:' + GULP_PORT);
     server({
         port: DOC_PORT,
         host: '0.0.0.0',
@@ -27,7 +27,7 @@ function makeServer(cb) {
             port: LIVERELOAD_PORT
         }
     });
-    console.log('Documentation server http://127.0.0.1:' + DOC_PORT);
+    fancyLog('Documentation server http://127.0.0.1:' + DOC_PORT);
     cb();
 }
 
