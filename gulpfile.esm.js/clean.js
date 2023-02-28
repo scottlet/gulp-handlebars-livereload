0e9d221da3537872d5d77c084ad2a3f72f899e5a
Scott van Looy <scott@quul.net>
2023-02-28 11:24:46 +0100

    Updated deps

--- gulpfile.esm.js/clean.js
+++ gulpfile.esm.js/clean.js
@@ -1,11 +1,10 @@
-import { sync } from 'del';
+import { deleteAsync } from 'del';
 import { CONSTS } from './CONSTS';
 
 const { DIST_DEST, DEPLOY_TARGET } = CONSTS;
 
-function clean(cb) {
-    sync([DIST_DEST, DEPLOY_TARGET]);
-    cb();
+function clean() {
+    return deleteAsync([DIST_DEST, DEPLOY_TARGET]);
 }
 
 export { clean };
