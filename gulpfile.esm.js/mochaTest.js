012fecc7cba26ad94ef9152470a9dffd6e2643ba
Scott van Looy <scott@quul.net>
2024-06-04 08:55:02 +0200

    Latest mochaTest

--- gulpfile.esm.js/mochaTest.js
+++ gulpfile.esm.js/mochaTest.js
@@ -6,7 +6,7 @@ import gulpWait from 'gulp-wait';
 import { CONSTS } from './CONSTS';
 import gulpChangedInPlace from 'gulp-changed-in-place';
 
-const { TESTS_PATH, GULPFILE } = CONSTS;
+const { SRC, GULPFILE } = CONSTS;
 
 const TEST_DELAY = 3050;
 
@@ -19,14 +19,14 @@ if (process.env.NODE_ENV === 'production') {
   mochaOptions.R = 'nyan';
 }
 
-const SRC = [`${TESTS_PATH}**/*.test.js`, `${GULPFILE}/**/*.test.js`];
+const TESTS_SRC = [`${SRC}/**/*.test.js`, `${GULPFILE}/**/*.test.js`];
 
 /**
  * Executes Mocha tests with a delay and notifies on errors.
  * @returns {NodeJS.ReadWriteStream} The Gulp stream with the Mocha test results.
  */
 function mochaTestLR() {
-  return src(SRC, {
+  return src(TESTS_SRC, {
     read: false
   })
     .pipe(gulpWait(TEST_DELAY))
@@ -44,7 +44,7 @@ function mochaTestLR() {
  * @returns {NodeJS.ReadWriteStream} The Gulp stream with the Mocha test results.
  */
 function mochaTest() {
-  return src(SRC, {
+  return src(TESTS_SRC, {
     read: false
   })
     .pipe(
